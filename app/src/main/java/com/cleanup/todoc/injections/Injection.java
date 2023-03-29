1e643138fab308bef28987f16686529ffd7e02dd
axelrbe <ribeiro.axel.fabien@gmail.com>
2023-03-29 16:05:27 +0200

    First commit

---
+++ app/src/main/java/com/cleanup/todoc/injections/Injection.java
@@ -0,0 +1,35 @@
+package com.cleanup.todoc.injections;
+
+import android.content.Context;
+
+import com.cleanup.todoc.database.TodocDatabase;
+import com.cleanup.todoc.repositories.ProjectRepository;
+import com.cleanup.todoc.repositories.TaskRepository;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Injection {
+
+    public static ProjectRepository getProjectRepository(Context context) {
+        TodocDatabase db = TodocDatabase.getInstance(context);
+        return new ProjectRepository(db.projectDAO());
+    }
+
+    public static TaskRepository getTaskRepository(Context context) {
+        TodocDatabase db = TodocDatabase.getInstance(context);
+        return new TaskRepository(db.taskDAO());
+    }
+
+    public static Executor getExecutor(Context context) {
+        return Executors.newSingleThreadExecutor();
+    }
+
+    public static ViewModelFactory getViewModelFactory(Context context) {
+        ProjectRepository projectRepository = getProjectRepository(context);
+        TaskRepository taskRepository = getTaskRepository(context);
+        Executor executor = getExecutor(context);
+        return new ViewModelFactory(projectRepository, taskRepository, executor);
+    }
+
+}
